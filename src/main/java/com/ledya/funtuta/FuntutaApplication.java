269154815cdbe2dad77610c8eeeba4c8df6eca31
LedyaBakloug <ledya.beshara@yahoo.com>
2023-10-07 11:15:10 -0400

    first commit

---
+++ src/main/java/com/ledya/funtuta/FuntutaApplication.java
@@ -0,0 +1,13 @@
+package com.ledya.funtuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuntutaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ledya/funtuta/FuntutaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledya.funtuta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
